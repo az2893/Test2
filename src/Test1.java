bb7df9e30bad1d687ba8c1f7b211018d8703b319
p01 <875382214@qq.com>
2018-06-06 15:09:54 +0800

    插入 hello Github字段

--- src/Test1.java
+++ src/Test1.java
@@ -4,8 +4,10 @@ public class Test1 {
    public static void  main(String args[]){
 
            Operation o =new AddOperation();
-          System.out.print(o.getResult(1,3));
+          System.out.println(o.getResult(1,3));
 
+
+          System.out.println("Hello GitHub! ");
 
     }
 
