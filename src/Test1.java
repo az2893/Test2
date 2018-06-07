83072cb0addc51f547939f54602e2b44d64ae1ba
p01 <875382214@qq.com>
2018-06-07 09:24:32 +0800

    测试了颜色，被修改时是蓝色的！！！！！！！

--- src/Test1.java
+++ src/Test1.java
@@ -15,16 +15,15 @@ public class Test1 {
       
           System.out.println("Hello GitHub 3 !");
       
+          System.out.println("Hello GitHub 4 !");
       
-        System.out.println("Hello GitHub 4 !");
-      
-      System.out.println("Hello GitHub 5 !");
+          System.out.println("Hello GitHub 5 !");
 
-       System.out.println("Hello GitHub 6 !");
+          System.out.println("Hello GitHub 6 !");
       
-      System.out.println("Hello GitHub 7 !");
+          System.out.println("Hello GitHub 7 !");
 
-      System.out.println("今天高考，测试一下不同状态下文件的颜色");
+          System.out.println("今天高考，测试一下不同状态下文件的颜色");
       // 被修改是蓝色
   }
 
